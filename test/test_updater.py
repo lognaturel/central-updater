16bc49e6e252dbe4c0ba8dcfa4b1c46d561049bc
Hélène Martin <lognaturel@gmail.com>
2022-05-17 17:04:19 -0700

    Update tests

--- test/test_updater.py
+++ test/test_updater.py
@@ -6,45 +6,45 @@ from updater import *
 
 @mock.patch("builtins.open", mock.mock_open(read_data='{"foo": "bar"}'))
 def test_get_config__returns_dict_from_json():
-    assert get_config() == {'foo': 'bar'}
+    assert get_config("config.json") == {'foo': 'bar'}
 
 
 @mock.patch("builtins.open", side_effect=FileNotFoundError)
 def test_get_config__raises_error__when_file_not_found(open_mock):
     with pytest.raises(FileNotFoundError):
-        get_config()
+        get_config("config.json")
 
 
 @mock.patch("builtins.open", mock.mock_open(read_data='{"token": "foo"}'))
 @mock.patch("requests.get")
 def test_get_verified_cached_token__returns_cached_token__when_token_valid(request_mock):
     request_mock.return_value.ok = True
-    assert get_verified_cached_token({"url": "bar"}) == "foo"
+    assert get_verified_cached_token({"url": "bar"}, "cache") == "foo"
 
 
 @mock.patch("builtins.open", mock.mock_open(read_data='{"token": "foo"}'))
 @mock.patch("requests.get")
 def test_get_verified_cached_token__returns_none__when_token_not_valid(request_mock):
     request_mock.return_value.ok = False
-    assert get_verified_cached_token({"url": "bar"}) is None
+    assert get_verified_cached_token({"url": "bar"}, "cache") is None
 
 
 @mock.patch("builtins.open", side_effect=FileNotFoundError)
 def test_get_verified_cached_token__returns_none__when_no_cache_file(open_mock):
-    assert get_verified_cached_token({"url": "bar"}) is None
+    assert get_verified_cached_token({"url": "bar"}, "cache") is None
 
 
 @mock.patch("builtins.open", mock.mock_open(read_data='{"foo": "bar"}'))
 @mock.patch("requests.get")
 def test_get_verified_cached_token__returns_none__when_cache_file_corrupt(request_mock):
     request_mock.return_value.ok = True
-    assert get_verified_cached_token({"url": "bar"}) is None
+    assert get_verified_cached_token({"url": "bar"}, "cache") is None
 
 
 @mock.patch("updater.get_verified_cached_token")
 def test_get_token__returns_cached_token__when_cached_token_verified(verify_fn):
     verify_fn.return_value = "foo"
-    assert get_token({"url": "bar"}) is "foo"
+    assert get_token({"url": "bar"}, "cache") == "foo"
 
 
 @mock.patch("updater.get_new_token")
@@ -59,7 +59,7 @@ def test_get_token__does_not_request_new_token__when_cached_token_verified(verif
 def test_get_token__requests_new_token__when_no_cached_verified_token(verify_fn, get_new_fn):
     verify_fn.return_value = None
     get_new_fn.return_value = "foo"
-    assert get_token({"url": "bar"}) is "foo"
+    assert get_token({"url": "bar"}) == "foo"
 
 
 @mock.patch("updater.get_new_token")
@@ -84,19 +84,20 @@ def test_write_to_cache__creates_file__when_no_file_exists(open_mock, json_mock)
     open_mock.side_effect = mocked_open
     open_mock.__enter__ = mock.Mock(return_value=(mock.Mock(), None))
     open_mock.__exit__ = mock.Mock(return_value=None)
-    write_to_cache("foo", "bar")
+    write_to_cache("cache", "foo", "bar")
     json_mock.assert_called_with({'foo': 'bar'}, mock.ANY)
 
 
 @mock.patch("json.dump")
 @mock.patch("builtins.open", mock.mock_open(read_data='{"foo": "foo"}'))
 def test_write_to_cache__replaces_token__when_file_with_token_exists(json_mock):
-    write_to_cache("foo", "bar")
+    write_to_cache("cache", "foo", "bar")
     json_mock.assert_called_with({'foo': 'bar'}, mock.ANY)
 
 
 @mock.patch("json.dump")
 @mock.patch("builtins.open", mock.mock_open(read_data='{"foo": "bar"}'))
 def test_write_to_cache__preserves_other_keys__when_file_with_other_keys_exists(json_mock):
-    write_to_cache("bar", "baz")
+    write_to_cache("cache", "bar", "baz")
     json_mock.assert_called_with({'bar': 'baz', 'foo': 'bar'}, mock.ANY)
+
